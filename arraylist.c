9370275e9a96b1c241f26499e9dfe6ec62f048d1
Replit user <>
2023-03-30 16:26:55 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -35,7 +35,7 @@ void push(ArrayList * l, void * data, int i){
   if(l->size == l->capacity)
   {
     l->capacity *= 2;
-    l->data = realloc(l->data, l->capacity);
+    l->data = realloc(l->data, sizeof(void *) * l->capacity);
   }
   for(int k = l->size; k > i; k++)
     {
