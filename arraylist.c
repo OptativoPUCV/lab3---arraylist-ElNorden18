9e4353c94cccc99deb1c68d55cf4921ee094610f
Replit user <>
2023-03-30 15:58:52 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -20,7 +20,7 @@ ArrayList *createList(void) {
 }
 
 void append(ArrayList * l, void * data){
-  *l->data[size] = *data;
+  *l->data[l->size] = *data;
 }
 
 void push(ArrayList * l, void * data, int i){
