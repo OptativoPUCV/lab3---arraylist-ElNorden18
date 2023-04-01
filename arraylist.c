d49fbfd7b29889be695f5b1adee1c5e84d72b2d9
Replit user <>
2023-04-01 02:41:53 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -46,7 +46,10 @@ void push(ArrayList * l, void * data, int i){
 }
 
 void* pop(ArrayList * l, int i){
-  if(l == NULL) return NULL;
+  if(l == NULL){
+    l->size = 0;
+    return NULL;
+  }
   if(i < 0)
     i = l->size+i;
   void * aux = l->data[i];
