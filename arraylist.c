f57547560852f93b43651a55433607671f9ea686
Replit user <>
2023-04-01 02:30:45 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -46,7 +46,8 @@ void push(ArrayList * l, void * data, int i){
 }
 
 void* pop(ArrayList * l, int i){
-  return NULL;
+  void * aux = l->data[i];
+  return aux;
 }
 
 void* get(ArrayList * l, int i){
