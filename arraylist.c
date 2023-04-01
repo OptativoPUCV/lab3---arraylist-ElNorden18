e6b764e7dbeed0b0702aa59aaec0f8a2fab94116
Replit user <>
2023-04-01 02:27:42 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -46,7 +46,11 @@ void push(ArrayList * l, void * data, int i){
 }
 
 void* pop(ArrayList * l, int i){
+  if(i >= l->size)
     return NULL;
+  if(i < 0)
+    return l->data[size-i];
+  return l->data[i];
 }
 
 void* get(ArrayList * l, int i){
